5aa69fdb2e8406dd6621f279989e27980ec1f893
Isaac Bunga <isaac.bunga@ideiasdinamicas.com>
2024-06-05 07:12:15 +0100

    feat:add Tables Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import {TableCompone} from "./shared/components/tables/tables.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,TableCompone],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
